d21634ea3496809ca3402ef2ee2dad54ddbbc770
Caio Chaves <caio.chaves.ribeiro@gmail.com>
2021-06-07 10:27:26 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First(); // provisorio
             _context.Add(obj);
             _context.SaveChanges();
         }
